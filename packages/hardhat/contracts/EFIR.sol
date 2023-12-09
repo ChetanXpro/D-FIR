cced15167db0a881e31e2f126c434fc39995db31
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-12-09 05:50:39 +0530

    updated burn + naming variables

--- packages/hardhat/contracts/EFIR.sol
+++ packages/hardhat/contracts/EFIR.sol
@@ -8,12 +8,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 contract EFIR is ERC721, ERC721URIStorage, ERC721Burnable {
     uint256 private _currentFIRId;
     uint256[] private s_firIds;
-    mapping(uint256 => address) public firApprovedToOfficer;
-    mapping(uint256 => address) public assignedOfficer;
+    mapping(uint256 => address) public s_firApprovedToOfficer;
+    mapping(uint256 => address) public s_assignedOfficer;
 
-    event FiledFIR(address indexed owner, uint256 tokenId, uint256 timeRecorded);
-    event AssignedOfficer(address indexed officer, uint256 tokenId, uint256 timeRecorded);
-    event UpdatedFIR(address indexed officer, uint256 tokenId, uint256 timeRecorded, string tokenUri);
+    event FiledFIR(address indexed owner, uint256 firId, uint256 timeRecorded);
+    event AssignedOfficer(address indexed officer, uint256 firId, uint256 timeRecorded);
+    event UpdatedFIR(address indexed officer, uint256 firId, uint256 timeRecorded, string tokenUri);
 
     constructor() ERC721("EFIRToken", "EFIRT") {
         _currentFIRId = 0;
@@ -30,40 +30,50 @@ contract EFIR is ERC721, ERC721URIStorage, ERC721Burnable {
 
     function approveOfficer(uint256 firId, address officer) public {
         require(ownerOf(firId) == msg.sender, "ERC721: caller is not the owner");
-        firApprovedToOfficer[firId] = officer;
+        s_firApprovedToOfficer[firId] = officer;
     }
 
     function assignOfficer(uint256 firId) public {
         require(_exists(firId), "ERC721: FIR does not exist");
-        address approvedOfficer = firApprovedToOfficer[firId];
+        address approvedOfficer = s_firApprovedToOfficer[firId];
         require(approvedOfficer == msg.sender, "ERC721: Not approved officer");
 
-        assignedOfficer[firId] = approvedOfficer;
+        s_assignedOfficer[firId] = approvedOfficer;
         _transfer(msg.sender, approvedOfficer, firId);
         emit AssignedOfficer(approvedOfficer, firId, block.timestamp);
     }
 
     function updateFIR(uint256 firId, string memory tokenUri) public {
-        require(assignedOfficer[firId] == msg.sender, "ERC721: Not authorized officer");
+        require(s_assignedOfficer[firId] == msg.sender, "ERC721: Not authorized officer");
         _setTokenURI(firId, tokenUri);
         emit UpdatedFIR(msg.sender, firId, block.timestamp, tokenUri);
     }
 
-    function burn(uint256 tokenId) public override {
-        _burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function _setTokenURI(uint256 tokenId, string memory tokenUri) internal override(ERC721URIStorage) {
-        super._setTokenURI(tokenId, tokenUri);
-    }
-
-    function _exists(uint256 tokenId) internal view returns (bool) {
+    function burn(uint256 firId) public override {
         for (uint256 i = 0; i < s_firIds.length; i++) {
-            if (s_firIds[i] == tokenId) {
+            if (s_firIds[i] == firId) {
+                // Remove firId from the array by swapping with the last element
+                s_firIds[i] = s_firIds[s_firIds.length - 1];
+                s_firIds.pop();
+                break; // Exit the loop once the swap is done
+            }
+        }
+        s_assignedOfficer[firId] = address(0);
+        s_firApprovedToOfficer[firId] = address(0);
+        _burn(firId);
+    }
+
+    function tokenURI(uint256 firId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(firId);
+    }
+
+    function _setTokenURI(uint256 firId, string memory tokenUri) internal override(ERC721URIStorage) {
+        super._setTokenURI(firId, tokenUri);
+    }
+
+    function _exists(uint256 firId) internal view returns (bool) {
+        for (uint256 i = 0; i < s_firIds.length; i++) {
+            if (s_firIds[i] == firId) {
                 return true;
             }
         }
