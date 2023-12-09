4de7a1058471f657590733738aa8df365d8a4018
chetan <chetan@wow.local>
2023-12-10 04:36:37 +0530

    add create wallet func

--- packages/nextjs/hooks/custom/useLightHouse.tsx
+++ packages/nextjs/hooks/custom/useLightHouse.tsx
@@ -1,5 +1,6 @@
 import lighthouse from "@lighthouse-web3/sdk";
 import AES from "crypto-js/aes";
+import { ethers } from "ethers";
 import { encodePacked, keccak256, toBytes } from "viem";
 import { AddressType } from "~~/types/abitype/abi";
 
@@ -8,7 +9,22 @@ const secretKey = process.env.NEXT_PUBLIC_LIGHTHOUSE_SECRET_KEY;
 
 const useStorage = (address: AddressType) => {
   const uploadEncryptedDataOnLighthouse = async (longerDescription: string): Promise<string> => {
-    const signedMessage = AES.encrypt(longerDescription, secretKey as string).toString();
+    console.log("======= LONGER DESCRIPTION=======", longerDescription);
+
+    // Generate a new wallet
+    const wallet = ethers.Wallet.createRandom();
+
+    // Display the new wallet's public address and private key
+    console.log("New Wallet Address:", wallet.address);
+    console.log("New Wallet Private Key:", wallet.privateKey);
+
+    // const message = "Unique message to sign"; // This message should be the one you want to encrypt and upload
+
+    const signedMessage = await wallet.signMessage(longerDescription);
+
+    console.log("Signed Message:", signedMessage);
+
+    // const signedMessage = AES.encrypt(longerDescription, secretKey as string).toString();
     const response = await lighthouse.textUploadEncrypted(
       longerDescription,
       apiKey as string,
@@ -17,7 +33,7 @@ const useStorage = (address: AddressType) => {
     );
     let fileHash = "";
     fileHash = response.data.hash;
-    console.log(fileHash);
+    console.log("======= FILE HASH=======", fileHash);
     return fileHash;
   };
 
@@ -25,6 +41,9 @@ const useStorage = (address: AddressType) => {
     shorterDescription: { name: string; district: string; description: string },
     longerDescription: string,
   ): Promise<string> => {
+    console.log("======= SHORTER DESCRIPTION=======", shorterDescription);
+    console.log("======= LONGER DESCRIPTION=======", longerDescription);
+
     let fileHash = await uploadEncryptedDataOnLighthouse(longerDescription);
     const metaDataJson = {
       name: shorterDescription.name + "Filed FIR about crime at: " + shorterDescription.district,
